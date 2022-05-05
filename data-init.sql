99f4ddc64f71463c2c0127820bb195d737c020d3
Qwanjai Deelertpradit <d_khonjai@hotmail.com>
2022-05-05 11:37:58 +0700

    correct syntax

--- data-init.sql
+++ data-init.sql
@@ -1,52 +1,55 @@
-CREATE TABLE roles (
+CREATE TABLE IF NOT EXISTS roles (
 	role_uuid VARCHAR ( 50 ) PRIMARY KEY,
 	role_name VARCHAR ( 50 ) NOT NULL
 );
 
-CREATE TABLE resources (
-	resources_uuid VARCHAR ( 50 ) PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS resources (
+	resource_uuid VARCHAR ( 50 ) PRIMARY KEY,
 	file_name VARCHAR ( 50 ) NOT NULL,
     path_name VARCHAR ( 255 )
 );
 
-CREATE TABLE categories (
+CREATE TABLE IF NOT EXISTS categories (
 	category_uuid VARCHAR ( 50 ) PRIMARY KEY,
 	category_name VARCHAR ( 50 ) NOT NULL
 );
 
-CREATE TABLE fdn_status (
+CREATE TABLE IF NOT EXISTS fdn_status (
 	status_uuid VARCHAR ( 50 ) PRIMARY KEY,
 	status_name VARCHAR ( 50 ) NOT NULL
 );
 
-CREATE TABLE target_categories (
-	target_categories_uuid VARCHAR ( 50 ) PRIMARY KEY,
-	target_categories_name VARCHAR ( 255 ) NOT NULL
+CREATE TABLE IF NOT EXISTS target_categories (
+	target_categoy_uuid VARCHAR ( 50 ) PRIMARY KEY,
+	target_category_name VARCHAR ( 255 ) NOT NULL
  );
  
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
 	user_uuid VARCHAR ( 50 ) PRIMARY KEY,
 	username VARCHAR ( 50 ) UNIQUE NOT NULL,
     email VARCHAR ( 255 ) UNIQUE NOT NULL,
     fname VARCHAR ( 255 ) NOT NULL,
     lname VARCHAR ( 255 ) NOT NULL, 
 	password VARCHAR ( 50 ) NOT NULL,
+    role_uuid VARCHAR ( 50 ) NOT NULL,
     FOREIGN KEY (role_uuid)
         REFERENCES roles (role_uuid)
 );
 
-CREATE TABLE user_posts (
+CREATE TABLE IF NOT EXISTS user_posts (
 	user_post_uuid VARCHAR ( 50 ) PRIMARY KEY,
 	post_title VARCHAR ( 255 ) NOT NULL,
     post_detail VARCHAR ( 255 ) NOT NULL,
     create_date TIMESTAMP NOT NULL,
-    FOREIGN KEY (resource_uuid)
-        REFERENCES resources (resource_uuid),
+    user_uuid VARCHAR ( 50 ) NOT NULL,
+    resource_uuid VARCHAR ( 50 ) NOT NULL,  
     FOREIGN KEY (user_uuid)
-        REFERENCES users (user_uuid)
+        REFERENCES users (user_uuid),
+    FOREIGN KEY (resource_uuid)
+        REFERENCES resources (resource_uuid)
 );
 
-CREATE TABLE foundations (
+CREATE TABLE IF NOT EXISTS  foundations (
 	fdn_uuid VARCHAR ( 50 ) PRIMARY KEY,
 	name VARCHAR ( 255 ) UNIQUE NOT NULL,
     address_no VARCHAR ( 255 ) NOT NULL,
@@ -62,32 +65,36 @@ CREATE TABLE foundations (
     establish_date DATE NOT NULL,
     email VARCHAR ( 50 ) UNIQUE NOT NULL,
     contact_num VARCHAR ( 20 ) NOT NULL,
+    status_uuid VARCHAR ( 50 ) NOT NULL,
+    resource_uuid VARCHAR ( 50 ) NOT NULL,
     FOREIGN KEY (status_uuid)
-        REFERENCES fdn_status (status_uuid)
+        REFERENCES fdn_status (status_uuid),
     FOREIGN KEY (resource_uuid)
-        REFERENCES resources (resource_uuid),
+        REFERENCES resources (resource_uuid)
 );
 
-CREATE TABLE fdns_resources_documents (
+CREATE TABLE IF NOT EXISTs  fdns_resources_documents (
 	fdn_resources_doc_uuid VARCHAR ( 50 ) PRIMARY KEY,
 	file_name VARCHAR ( 50 ) NOT NULL,
     file_path VARCHAR ( 255 ) NOT NULL,
     file_type VARCHAR ( 50 ) NOT NULL,
+    fdn_uuid VARCHAR ( 50 ) NOT NULL,
     FOREIGN KEY (fdn_uuid)
         REFERENCES foundations (fdn_uuid)
 );
 
-CREATE TABLE fdn_projects (
+CREATE TABLE IF NOT EXISTs  fdn_projects (
 	fdn_project_uuid VARCHAR ( 50 ) PRIMARY KEY,
 	fp_name VARCHAR ( 255 ) NOT NULL,
     start_date TIMESTAMP NOT NULL,
     end_date TIMESTAMP NOT NULL,
     goal INT NOT NULL,
+    fdn_uuid VARCHAR ( 50 ) NOT NULL,
     FOREIGN KEY (fdn_uuid)
         REFERENCES foundations (fdn_uuid)
 );
 
-CREATE TABLE fdn_categories (
+CREATE TABLE IF NOT EXISTS  fdn_categories (
   fdn_uuid VARCHAR (50)  NOT NULL,
   category_uuid VARCHAR (50) NOT NULL,
   PRIMARY KEY (fdn_uuid, category_uuid),
@@ -97,7 +104,7 @@ CREATE TABLE fdn_categories (
       REFERENCES categories (category_uuid)
 );
 
-CREATE TABLE volunteers_projects (
+CREATE TABLE IF NOT EXISTS  volunteers_projects (
 	volunteer_projects_uuid VARCHAR (50) PRIMARY KEY,
 	vp_name VARCHAR (255)  NOT NULL,
 	location VARCHAR (255) NOT NULL,
@@ -110,24 +117,24 @@ CREATE TABLE volunteers_projects (
   contact_detail TEXT NOT NULL,
   activity_type VARCHAR (255),
   fdn_uuid VARCHAR (50)  NOT NULL,
-  target_category_uuid VARCHAR (50)  NOT NULL,
+  target_categoy_uuid VARCHAR (50)  NOT NULL,
      FOREIGN KEY (fdn_uuid)
       REFERENCES foundations (fdn_uuid),
-     FOREIGN KEY (target_category_uuid)
-      REFERENCES target_category (target_category_uuid)
+     FOREIGN KEY (target_categoy_uuid)
+      REFERENCES target_categories (target_categoy_uuid)
 );
 
-CREATE TABLE volunteer_lists (
+CREATE TABLE IF NOT EXISTS  volunteer_lists (
   volunteer_list_uuid VARCHAR (50) PRIMARY KEY ,
-  volunteer_project_uuid VARCHAR (50) NOT NULL,
+  volunteer_projects_uuid VARCHAR (50) NOT NULL,
   user_uuid VARCHAR (50) NOT NULL,
-  FOREIGN KEY (volunteer_project_uuid)
-      REFERENCES volunteers_projects (volunteer_project_uuid),
+  FOREIGN KEY (volunteer_projects_uuid)
+      REFERENCES volunteers_projects (volunteer_projects_uuid),
   FOREIGN KEY (user_uuid)
       REFERENCES users (user_uuid)
 );
 
-CREATE TABLE fdn_posts (
+CREATE TABLE IF NOT EXISTS  fdn_posts (
   fdn_post_uuid VARCHAR (50) PRIMARY KEY ,
   post_title VARCHAR (255) NOT NULL,
   post_detail VARCHAR (255) NOT NULL,
