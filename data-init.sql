ab00b1865b39d45a5ede40b8107a937c2a87602a
Qwanjai Deelertpradit <d_khonjai@hotmail.com>
2022-05-24 17:47:56 +0700

    update properties

--- data-init.sql
+++ data-init.sql
@@ -117,14 +117,14 @@ CREATE TABLE IF NOT EXISTS  volunteers_projects (
   create_date TIMESTAMP NOT NULL,
   registration_url VARCHAR(255) NOT NULL,
   fdn_uuid VARCHAR (50)  NOT NULL,
-  target_categoy_uuid VARCHAR (50)  NOT NULL,
+  target_categoy_id VARCHAR (50)  NOT NULL,
    resource_uuid VARCHAR (50) NOT NULL,
-  FOREIGN KEY (resource_uuid)
+    FOREIGN KEY (resource_uuid)
       REFERENCES resources (resource_uuid),
-     FOREIGN KEY (fdn_uuid)
+    FOREIGN KEY (fdn_uuid)
       REFERENCES foundations (fdn_uuid),
-     FOREIGN KEY (target_categoy_uuid)
-      REFERENCES target_categories (target_categoy_uuid)
+    FOREIGN KEY (target_categoy_id)
+      REFERENCES target_categories (target_categoy_id)
 );
 
 
@@ -155,22 +155,22 @@ CREATE TABLE IF NOT EXISTS  fdn_staff (
 
 CREATE TABLE IF NOT EXISTS fdn_project_target_categories (
   fdn_project_uuid VARCHAR (50) NOT NULL,
-  target_category_uuid VARCHAR (50) NOT NULL ,
-    PRIMARY KEY (fdn_project_uuid, target_category_uuid),
+  target_category_id VARCHAR (50) NOT NULL ,
+    PRIMARY KEY (fdn_project_uuid, target_category_id),
   FOREIGN KEY (fdn_project_uuid)
       REFERENCES usersfdn_projects (fdn_project_uuid),
-  FOREIGN KEY (target_category_uuid)
-      REFERENCES target_categories (target_category_uuid)
+  FOREIGN KEY (target_category_id)
+      REFERENCES target_categories (target_category_id)
 );
 
 CREATE TABLE IF NOT EXISTS volunteer_project_target_categories (
   volunteer_project_uuid VARCHAR (50) NOT NULL,
-  target_category_uuid VARCHAR (50) NOT NULL ,
-    PRIMARY KEY (volunteer_project_uuid, target_category_uuid),
+  target_category_id VARCHAR (50) NOT NULL ,
+    PRIMARY KEY (volunteer_project_uuid, target_category_id),
   FOREIGN KEY (volunteer_project_uuid)
       REFERENCES volunteers_projects (volunteer_project_uuid),
-  FOREIGN KEY (target_category_uuid)
-      REFERENCES target_categories (target_category_uuid)
+  FOREIGN KEY (target_category_id)
+      REFERENCES target_categories (target_category_id)
 );
 
 INSERT INTO "roles" ("role_id", "role_name") VALUES ('1', 'ROLE_ADMIN');
